7429717103bd5f4217ac1a0999fa3c7622595755
nguyenscg <nguyencsg@gmail.com>
2025-01-21 18:33:58 -0600

    create mock_data.json with player stats that will be used for project

--- main.py
+++ main.py
@@ -6,3 +6,24 @@
 # Player Data should have puuid (just an id #), gameName, tagLine, average_kills, average_deaths, win_rate %, total_matches, and most_played_agent
 
 # Start with creating a mock_data.json, will sign up for production key later for Riot API
+
+# first import json library
+import json
+
+# create mock_data into .json
+new_data = [
+    {
+        "puuid": "12345",
+        "gameName": "Soli",
+        "tagLine": "awice",
+        "average_kills": 18,
+        "average_deaths": 10,
+        "win_rate": 66.67,
+        "total_matches": 132,
+        "most_played_agents": "Jett",
+    }
+]
+
+# save/create the new data into a mock_data.json
+with open("mock_data.json", "w") as data_file:
+    json.dump(new_data, data_file)
