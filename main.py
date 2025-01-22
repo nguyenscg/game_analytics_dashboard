70d8159949245289d3c97800edae03e486831d20
nguyenscg <nguyencsg@gmail.com>
2025-01-21 18:28:39 -0600

    pseudocode outline of what the project will be like

---
+++ main.py
@@ -0,0 +1,8 @@
+# Game Analytics Dashboard
+# Python based project that will showcase a player's statistics, match history, and win-rate
+
+
+# Player overview: This will display the player's data
+# Player Data should have puuid (just an id #), gameName, tagLine, average_kills, average_deaths, win_rate %, total_matches, and most_played_agent
+
+# Start with creating a mock_data.json, will sign up for production key later for Riot API
