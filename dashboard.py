e262a87ca3b265559448a08b7104fd8cdbdca44f
nguyenscg <nguyencsg@gmail.com>
2025-01-28 12:21:08 -0600

    created a search route and successfully display 'Hello World'

--- dashboard.py
+++ dashboard.py
@@ -12,10 +12,16 @@ except FileNotFoundError:
     data = []
     print("Error: mock_data.json file not found.")
 
+# home route
 @app.route("/")
 def hello_world():
     current_year = datetime.datetime.now().year
     return render_template("index.html", year=current_year, players=data)
+
+# add a search route that allows the user to 'search' for a player and display their stats
+@app.route("/search")
+def search():
+    return "<h1>Hello World!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
