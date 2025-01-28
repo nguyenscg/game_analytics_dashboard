8a292d713434675d28979c5ee9d3bb007945673a
nguyenscg <nguyencsg@gmail.com>
2025-01-28 11:39:26 -0600

    created a flask app. added a basic template and added a footer

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world!</p>"
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", year=current_year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
