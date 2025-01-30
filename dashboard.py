f9d7ee3e3227a3c07414df0910b8d3d66acb9495
nguyenscg <nguyencsg@gmail.com>
2025-01-30 15:55:56 -0600

    re-render

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import datetime
 import json
+import os
 
 app = Flask(__name__)
 
@@ -33,4 +34,5 @@ def search():
     return render_template("search.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
